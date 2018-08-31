ff8c0bdb04fb9c7d6d05e0f86c082b9292806ec8
Shyam Manchoju <shyammanchoji@gmail.com>
2018-08-31 18:35:20 +0530

    adding socket io connect and disconnect

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const hbs = require('hbs');
 const path =require('path');
+const socketIO = require('socket.io');
+const http = require('http');
 
 const port = process.env.PORT || 3000;
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine','hbs');
@@ -11,6 +15,15 @@ app.set('view engine','hbs');
 const publicPath =path.join(__dirname, '../public');
 
 app.use(express.static(publicPath));
+
+io.on('connect',(socket)=>{
+    console.log('New User connection');
+    socket.on('disconnect',()=>{
+        console.log('connection closed');
+    });
+});
+
+
 
 app.get('/',(req, res)=>{
     /*res.render('home.hbs',{
@@ -20,7 +33,7 @@ app.get('/',(req, res)=>{
     res.send('welcome to chat app');
   });
 
-app.listen(port,()=>{
+server.listen(port,()=>{
     console.log('Server started on port 3000')
 });
 console.log(__dirname + '/../public');
