eaf64e6bd1aa44a18f3fcb9c1bfd877ff7e8537d
Shyam Manchoju <shyammanchoji@gmail.com>
2018-09-09 21:38:15 +0530

    adding individual room messages

--- server/server.js
+++ server/server.js
@@ -50,13 +50,19 @@ io.on('connect',(socket)=>{
 
     // Create a message on emit from client 
     socket.on('createMessage', (message, callback)=>{
-        //console.log('Message :', message);
-        io.emit('newMessage', generateMessage(message.from, message.messageText));
+        var user = users.getUser(socket.id);
+        if(user && isRealString(message.messageText)){
+            io.to(user.room).emit('newMessage', generateMessage(user.name, message.messageText));
+        }
+       
         callback();
     });
 
     socket.on('createLocationMessage', (coords)=>{
-        io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));  
+        var user = users.getUser(socket.id);
+        if(user){
+            io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, coords.latitude, coords.longitude));  
+        }
     });
 
     socket.on('disconnect',()=>{
